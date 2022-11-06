08094e4b6b5b82476f580b3064029554b0495670
Artsiom Koptsiukh <artsiom.koptsiukh@customertimes.com>
2022-11-06 12:00:01 +0400

    added search test

--- src/test/java/tests/AndroidSearchTests.java
+++ src/test/java/tests/AndroidSearchTests.java
@@ -1,14 +1,19 @@
 package tests;
 
+import com.codeborne.selenide.CollectionCondition;
 import io.appium.java_client.AppiumBy;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.CollectionCondition.texts;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static io.qameta.allure.Allure.step;
 
 public class AndroidSearchTests extends TestBase {
+
     @Test
     void searchTest() {
         step("Type search", () -> {
@@ -20,4 +25,22 @@ public class AndroidSearchTests extends TestBase {
                 $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title"))
                         .shouldHave(sizeGreaterThan(0)));
     }
+
+    @Test
+    @DisplayName("Search and open article test")
+    void searchAndOpenArticleTest() {
+
+        step("Type search", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/search_container")).click();
+            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Java");
+        });
+        step("Verify content found", () -> {
+            $$(AppiumBy.id("org.wikipedia.alpha:id/search_results_list")).shouldHave(
+                    CollectionCondition.sizeGreaterThan(0));
+        });
+        step("Open article about Java ", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title")).click();
+        });
+    }
 }
+
